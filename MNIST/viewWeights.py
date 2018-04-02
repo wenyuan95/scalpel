a2171d7a04c9ebe1c0654dd293e84c44bd636910
Wen Yuan <wenyuan@Wens-MacBook-Pro.local>
2018-04-02 19:53:43 +0100

    function of graph plot

--- MNIST/viewWeights.py
+++ MNIST/viewWeights.py
@@ -1,6 +1,7 @@
 import torch
 import argparse
 import matplotlib.pyplot as plt
+import numpy as np
 
 def get_cmd_opts():
   parser = argparse.ArgumentParser(description='PyTorch model viewer')
@@ -22,19 +23,27 @@ def extract_weights(model, layer, weight='kernel'):
   
   raise Exception('Only kernel, bias, alpha, beta  value can be extracted from the model.')
 
+
+def view(model, layer, weight, mode='raw'):
+  weight_map = extract_weights(model, layer, weight)
+  if model == 'pruned':
+    alpha = extract_weights(model, layer, weight)
+
+  output_dim = weight_map.shape[0]
+  weight_in_2D = weight_map.view(output_dim,-1)
+  #plt.matshow(weight_in_2D.numpy().transpose())
+  plt.matshow(weight_in_2D.numpy())
+  plt.show()
+
 def main():
   args = get_cmd_opts()
 
   model = torch.load(args.model)
 
   weight_map = extract_weights(model, 'conv1', 'weight')
-  print weight_map.squeeze().numpy().shape
-  y = torch.zeros(20,25)
-  y = weight_map.view(20,25)
-  print y.shape
-  plt.matshow(y.numpy().transpose())
+  alpha = extract_weights(model, 'conv1', 'alpha')
 
-  plt.show()
+  view(model, 'conv2', 'weight')
 
 if __name__ == "__main__":
   main()
